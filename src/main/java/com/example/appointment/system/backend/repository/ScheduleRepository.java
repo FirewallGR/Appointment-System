37ff39710cc8baa73a62583202fecac81b796d97
FirewallGR <qwert.ting@bk.ru>
2024-12-27 15:26:51 +0300

    хз

--- src/main/java/com/example/appointment/system/backend/repository/ScheduleRepository.java
+++ src/main/java/com/example/appointment/system/backend/repository/ScheduleRepository.java
@@ -3,9 +3,12 @@ package com.example.appointment.system.backend.repository;
 import com.example.appointment.system.backend.model.Schedule;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.time.LocalDate;
 import java.util.List;
 import java.util.UUID;
 
 public interface ScheduleRepository extends JpaRepository<Schedule, UUID> {
     List<Schedule> findByDoctorId(UUID doctorId);
+
+    List<Schedule> findByDate(LocalDate date);
 }
